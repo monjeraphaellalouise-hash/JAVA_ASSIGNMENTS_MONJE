ca1555efde9aa4a7d49951f7a5062fc3395773ec
monjeraphaellalouise-hash <monjeraphaellalouise@gmail.com>
2026-02-23 21:51:25 +0800

    hobby

---
+++ JAVA FUNDAMENTALS/assignmentjavafunda01/Hobby.java
@@ -0,0 +1,8 @@
+public class Hobby {
+
+    public static void main(String[] args) {
+        System.out.println("My favorite hobby is swimming.");
+        System.out.println("I go swimming once a week. It helps me stay healthy and active.");
+        System.out.println("I enjoy swimming because it relaxes my mind. It also helps relieve stress after a busy day.");
+    }
+}
